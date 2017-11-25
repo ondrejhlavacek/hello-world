8fd341fb3276b87d090b176c2a2bbcfe40e4b112
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-25 12:39:03 +0100

    fixed tests

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -3,4 +3,13 @@ namespace PosledniSobota;
 
 class HelloWorld
 {
+    private $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    public function sayHello()
+    {
+        return 'Hello ' . $this->name;
+    }
 }
